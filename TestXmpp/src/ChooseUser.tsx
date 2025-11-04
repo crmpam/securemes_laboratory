a13f786192fd35b438aa35f2e39f552455768c4a
adolfo.gomez <adolfo.gomez@crmpam.com>
2025-11-04 17:09:55 +0100

    Solucionado problema en la navegación

--- TestXmpp/src/ChooseUser.tsx
+++ TestXmpp/src/ChooseUser.tsx
@@ -6,8 +6,8 @@ export const ChooseUser = () => {
   const navigation = useNavigation();
   return (
     <View>
-      <Button title="Usuario A" onPress={() => navigation.navigate("", { username: "test_crmpam_a", password: "testcrmpama" })} />
-      <Button title="Usuario B" onPress={() => navigation.navigate("", { username: "test_crmpam_b", password: "testcrmpamb" })} />
+      <Button title="Usuario A" onPress={() => navigation.navigate("Profile", { username: "test_crmpam_a", password: "testcrmpama" })} />
+      <Button title="Usuario B" onPress={() => navigation.navigate("Profile", { username: "test_crmpam_b", password: "testcrmpamb" })} />
     </View>
   );
 };
